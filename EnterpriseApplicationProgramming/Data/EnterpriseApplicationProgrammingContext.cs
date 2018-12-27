bb8d0a5de4c62cf3fd70d676f2010b96e24d0bff
Quang Hung <quanghungleo@gmail.com>
2018-12-27 11:27:20 +0700

    Hello

---
+++ EnterpriseApplicationProgramming/Data/EnterpriseApplicationProgrammingContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnterpriseApplicationProgramming.Models
+{
+    public class EnterpriseApplicationProgrammingContext : DbContext
+    {
+        public EnterpriseApplicationProgrammingContext (DbContextOptions<EnterpriseApplicationProgrammingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EnterpriseApplicationProgramming.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ EnterpriseApplicationProgramming/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EnterpriseApplicationProgramming.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
